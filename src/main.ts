46d6bc0df9cbd4518fb29eb25e032c7c716a691c
Tyree Carpenter <carpentertd@vcu.edu>
2025-10-13 11:26:51 -0400

    adding a couple packages to maybe fix build

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from "vue";
-import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
+import PrimeVue from "primevue/config";
+import { createApp } from "vue";
 import App from "./App.vue";
 
 const app = createApp(App);
 
 app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-  },
+	theme: {
+		preset: Aura,
+	},
 });
 
 app.mount("#app");
