28620288a227f2c8716552b0f7e993830db75828
Tyree Carpenter <carpentertd@vcu.edu>
2025-10-13 11:40:08 -0400

    quick ci-cd test

--- src/components/panels/MonthThreePanel.vue
+++ src/components/panels/MonthThreePanel.vue
@@ -1,25 +1,31 @@
 <script setup lang="ts">
-import TabPanel from 'primevue/tabpanel'
+import TabPanel from "primevue/tabpanel";
 
 defineProps<{
-  accounts: {
-    checkings: { label: string; amount: number }
-    savings: { label: string; amount: number }
-    brokerage: { label: string; amount: number }
-    creditCard: { label: string; amount: number }
-  }
-}>()
+	accounts: {
+		checkings: { label: string; amount: number };
+		savings: { label: string; amount: number };
+		brokerage: { label: string; amount: number };
+		creditCard: { label: string; amount: number };
+	};
+}>();
 </script>
 
 <template>
   <TabPanel value="november">
     <h1>Month Three</h1>
     <h2>
-      This month your checking account is going to go up {{ accounts.checkings.amount }} dollars
+      This month your checking account is going to go up
+      {{ accounts.checkings.amount }} dollars
     </h2>
-    <h2>This month your savings account is going to go up {{ accounts.savings.amount }} dollars</h2>
     <h2>
-      This month your brokerage account is going to go up {{ accounts.brokerage.amount }} dollars
+      This month your savings account is going to go up
+      {{ accounts.savings.amount }} dollars
     </h2>
+    <h2>
+      This month your brokerage account is going to go up
+      {{ accounts.brokerage.amount }} dollars
+    </h2>
+    <h1>Quick CI/CD Test</h1>
   </TabPanel>
 </template>
