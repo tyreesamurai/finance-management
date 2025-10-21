de1fcd4e71edb2affa13db522c937283c3e7a8a8
Tyree Carpenter <carpentertd@vcu.edu>
2025-10-21 16:15:35 -0400

    test commit

--- src/components/panels/MonthThreePanel.vue
+++ src/components/panels/MonthThreePanel.vue
@@ -27,6 +27,6 @@ defineProps<{
       {{ accounts.brokerage.amount }} dollars
     </h2>
     <h1>Quick CI/CD Test</h1>
-    <h1>Literally from today</h1>
+    <h1>Hi / Please work !:)</h1>
   </TabPanel>
 </template>
